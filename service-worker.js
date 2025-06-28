0043fd0682e45cb145bbf8f1a5e44c49bd8db9da
PaulaRomanazzi <romanazzipaula@hotmail.com>
2025-06-28 14:23:18 -0300

    Agrego service worker para soporte offline

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+﻿const CACHE_NAME = "hiemi-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/DeterminacionesHiemi-1.html"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
